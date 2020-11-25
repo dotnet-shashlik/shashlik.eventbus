360f14e6781640c10eb1e7fa688b989df4933a86
gucao <280780363@qq.com>
2020-11-25 20:12:17 +0800

    add MemoryQueue

---
+++ src/Shashlik.EventBus.MemoryQueue/MemoryEventSubscriber.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using Shashlik.Utils.Extensions;
+
+namespace Shashlik.EventBus.MemoryQueue
+{
+    public class MemoryEventSubscriber : IEventSubscriber
+    {
+        public MemoryEventSubscriber(IMessageSender messageSender, Logger<MemoryEventSubscriber> logger)
+        {
+            Logger = logger;
+            MessageSender = messageSender as MemoryMessageSender;
+        }
+
+        private MemoryMessageSender MessageSender { get; }
+        private Logger<MemoryEventSubscriber> Logger { get; }
+
+        public void Subscribe(IMessageListener listener, CancellationToken token)
+        {
+            MessageSender.OnMessageReceived += (sender, e) =>
+            {
+                MessageTransferModel message = e.MessageTransferModel;
+
+                if (message == null)
+                {
+                    Logger.LogError("[EventBus-Memory] deserialize message from rabbit error.");
+                    return;
+                }
+
+                if (message.EventName != listener.Descriptor.EventName)
+                {
+                    Logger.LogError(
+                        $"[EventBus-Memory] received invalid event name \"{message.EventName}\", expect \"{listener.Descriptor.EventName}\".");
+                    return;
+                }
+
+                Logger.LogDebug(
+                    $"[EventBus-Memory] received msg: {message.ToJson()}.");
+
+                while (true)
+                {
+                    try
+                    {
+                        // 处理消息
+                        listener.OnReceive(message, token);
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.LogError(ex,
+                            $"[EventBus-Memory] received msg execute OnReceive error: {message.ToJson()}.");
+                    }
+
+                    Thread.Sleep(20);
+                }
+            };
+        }
+    }
+}
---
+++ src/Shashlik.EventBus.MemoryQueue/MemoryMessageSender.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Shashlik.EventBus.MemoryQueue
+{
+    /// <summary>
+    /// 消息发送处理类
+    /// </summary>
+    public class MemoryMessageSender : IMessageSender
+    {
+        internal event OnMessageReceivedHandler OnMessageReceived;
+
+        public Task Send(MessageTransferModel message)
+        {
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                OnMessageReceived?.Invoke(this, new OnMessageTransferEventArgs
+                {
+                    MessageTransferModel = message
+                });
+            });
+
+            return Task.CompletedTask;
+        }
+    }
+}
