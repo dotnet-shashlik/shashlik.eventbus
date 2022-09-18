64a58f6379ca35b05d858a9cadf5fb4c423508a6
gucao <280780363@qq.com>
2022-09-18 17:38:11 +0800

    move dashboard demo

---
+++ src/samples/Shashlik.Dashboard.Demo/TokenCookieAuth.cs
@@ -0,0 +1,35 @@
+﻿using Shashlik.EventBus.Dashboard;
+
+namespace Shashlik.Dashboard.Demo
+{
+    public class TokenCookieAuth : IEventBusDashboardAuthentication
+    {
+        private const string Token = "token123";
+
+        public async Task<bool> AuthenticateAsync(HttpContext context)
+        {
+            await Task.CompletedTask;
+            var token = "";
+            if (context.Request.Query.ContainsKey("token"))
+            {
+                token = context.Request.Query["token"];
+            }
+            else if (context.Request.Cookies.ContainsKey("token"))
+            {
+                token = context.Request.Cookies["token"];
+            }
+
+            if (token == Token)
+            {
+                if (!context.Request.Cookies.ContainsKey("token"))
+                {
+                    context.Response.Cookies.Append("token", token);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
