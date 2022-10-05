79f42fd4319a811aa3098b314c485c98800676d6
gucao <280780363@qq.com>
2022-10-05 19:36:10 +0800

    no msg

--- src/Shashlik.EventBus.Redis/EventBusRedisMQOptions.cs
+++ src/Shashlik.EventBus.Redis/EventBusRedisMQOptions.cs
@@ -13,12 +13,12 @@ public class EventBusRedisMQOptions
         s => s.GetService<RedisClient>();
 
     /// <summary>
-    /// 消息堆积最大数量,默认0
+    /// 消息堆积最大数量,默认0不限制
     /// </summary>
     public int MaxLength { get; set; }
 
     /// <summary>
-    /// 消息堆积最大数量动态配置器,优先级比更高
+    /// 消息堆积最大数量动态配置器,优先级比<see cref="MaxLength"/>更高,这个可以根据不同的事件配置不同的堆积数量
     /// </summary>
     public Func<MessageTransferModel, int>? MaxLengthFactory { get; set; }
 }
