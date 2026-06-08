d84d9c223ad17dc69dc5c96f0b9e70520f51aec5
licas.li <licas.li@hg2mes.com>
2026-06-08 16:08:21 +0800

    refactor(relation-db): unify RDBMS initializers via FreeSql CodeFirst
    - New abstract base RelationDbMessageStorageInitializerBase (in
      Shashlik.EventBus.RelationDbStorage) that uses IFreeSqlFactory +
      CodeFirst.SyncStructure to auto-create tables and indexes from the
      entity model attributes added in the previous commit.
    - 4 dialect-specific *MessageStorageInitializer classes become 1-line
      shells that just pass the IFreeSqlFactory to the base ctor. The old
      raw-SQL DDL blocks (CREATE TABLE / CREATE SCHEMA / IF OBJECT_ID ...)
      are deleted.
    - Table name / schema / charset remain controlled by each
      *FreeSqlFactory.Aop.ConfigEntity handler, so the previous
      per-package table-name option continues to work.
    - The base class is virtual; subclasses can override InitializeAsync to
      add custom views/seed/partition etc. without touching the schema-sync
      path.

--- src/Shashlik.EventBus.PostgreSQL/PostgreSQLMessageStorageInitializer.cs
+++ src/Shashlik.EventBus.PostgreSQL/PostgreSQLMessageStorageInitializer.cs
@@ -1,69 +1,10 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Options;
-using Npgsql;
+﻿using Shashlik.EventBus.RelationDbStorage;
 
-namespace Shashlik.EventBus.PostgreSQL
+namespace Shashlik.EventBus.PostgreSQL;
+
+internal class PostgreSQLMessageStorageInitializer : RelationDbMessageStorageInitializerBase
 {
-    internal class PostgreSQLMessageStorageInitializer : IMessageStorageInitializer
+    public PostgreSQLMessageStorageInitializer(IFreeSqlFactory freeSqlFactory) : base(freeSqlFactory)
     {
-        public PostgreSQLMessageStorageInitializer(IOptionsMonitor<EventBusPostgreSQLOptions> options,
-            IConnectionString connectionString)
-        {
-            Options = options;
-            ConnectionString = connectionString;
-        }
-
-        private IOptionsMonitor<EventBusPostgreSQLOptions> Options { get; }
-        private IConnectionString ConnectionString { get; }
-
-        public async Task InitializeAsync(CancellationToken cancellationToken = default)
-        {
-            // 9.2及以下不支持CREATE SCHEMA IF NOT EXISTS，只能使用public
-            var sql = $@"
-{(Options.CurrentValue.Schema != "public" ? "CREATE SCHEMA IF NOT EXISTS " + Options.CurrentValue.Schema + ";" : "")}
-
-CREATE TABLE IF NOT EXISTS {Options.CurrentValue.FullPublishedTableName}(
-    ""id"" SERIAL PRIMARY KEY,    
-    ""msgId"" varchar(32) COLLATE ""pg_catalog"".""default"" NOT NULL,
-    ""environment"" varchar(32) COLLATE ""pg_catalog"".""default"",
-    ""eventName"" varchar(255) COLLATE ""pg_catalog"".""default"" NOT NULL,
-    ""eventBody"" text COLLATE ""pg_catalog"".""default"" NOT NULL,
-    ""createTime"" int8 NOT NULL,
-    ""delayAt"" int8 NOT NULL,
-    ""expireTime"" int8 NOT NULL,
-    ""eventItems"" text COLLATE ""pg_catalog"".""default"",
-    ""status"" varchar(32) COLLATE ""pg_catalog"".""default"" NOT NULL,
-    ""retryCount"" int4 NOT NULL,
-    ""isLocking"" bool NOT NULL,
-    ""lockEnd"" int8 NOT NULL,
-	UNIQUE (""msgId"")
-);
-
-CREATE TABLE IF NOT EXISTS {Options.CurrentValue.FullReceivedTableName}(
-    ""id"" SERIAL PRIMARY KEY,
-    ""msgId"" varchar(32) COLLATE ""pg_catalog"".""default"" NOT NULL,
-    ""environment"" varchar(32) COLLATE ""pg_catalog"".""default"",
-    ""eventName"" varchar(255) COLLATE ""pg_catalog"".""default"" NOT NULL,
-    ""eventHandlerName"" varchar(255) COLLATE ""pg_catalog"".""default"" NOT NULL,
-    ""eventBody"" text COLLATE ""pg_catalog"".""default"" NOT NULL,
-    ""createTime"" int8 NOT NULL,
-	""isDelay"" bool NOT NULL,
-    ""delayAt"" int8 NOT NULL,
-    ""expireTime"" int8 NOT NULL,
-    ""eventItems"" text COLLATE ""pg_catalog"".""default"",
-    ""status"" varchar(32) COLLATE ""pg_catalog"".""default"" NOT NULL,
-    ""retryCount"" int4 NOT NULL,
-    ""isLocking"" bool NOT NULL,
-    ""lockEnd"" int8 NOT NULL,
-	 UNIQUE (""msgId"", ""eventHandlerName"")
-);
-";
-            await using var connection = new NpgsqlConnection(ConnectionString.ConnectionString);
-            await connection.OpenAsync(cancellationToken).ConfigureAwait(false);
-            await using var cmd = connection.CreateCommand();
-            cmd.CommandText = sql;
-            await cmd.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
-        }
     }
 }
--- src/Shashlik.EventBus.SqlServer/SqlServerMessageStorageInitializer.cs
+++ src/Shashlik.EventBus.SqlServer/SqlServerMessageStorageInitializer.cs
@@ -1,79 +1,10 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Options;
+﻿using Shashlik.EventBus.RelationDbStorage;
 
-namespace Shashlik.EventBus.SqlServer
+namespace Shashlik.EventBus.SqlServer;
+
+public class SqlServerMessageStorageInitializer : RelationDbMessageStorageInitializerBase
 {
-    public class SqlServerMessageStorageInitializer : IMessageStorageInitializer
+    public SqlServerMessageStorageInitializer(IFreeSqlFactory freeSqlFactory) : base(freeSqlFactory)
     {
-        public SqlServerMessageStorageInitializer(IOptionsMonitor<EventBusSqlServerOptions> options,
-            IConnectionString connectionString)
-        {
-            Options = options;
-            ConnectionString = connectionString;
-        }
-
-        private IOptionsMonitor<EventBusSqlServerOptions> Options { get; }
-        private IConnectionString ConnectionString { get; }
-
-        public async Task InitializeAsync(CancellationToken cancellationToken = default)
-        {
-            var sql = $@"
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = '{Options.CurrentValue.Schema}')
-BEGIN
-	EXEC('CREATE SCHEMA [{Options.CurrentValue.Schema}]')
-END;
-
-IF OBJECT_ID(N'{Options.CurrentValue.FullPublishedTableName}',N'U') IS NULL
-BEGIN
-	CREATE TABLE {Options.CurrentValue.FullPublishedTableName}
-	(
-		[id] BIGINT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-		[msgId] VARCHAR(32) NOT NULL,
-		[environment] VARCHAR(32),
-		[eventName] VARCHAR(255) NOT NULL,
-		[eventBody] NVARCHAR(MAX) NOT NULL,
-		[createTime] BIGINT NOT NULL,
-		[delayAt] BIGINT NOT NULL,
-		[expireTime] BIGINT NOT NULL,
-		[eventItems] NVARCHAR(MAX) NULL,
-		[status] VARCHAR(32) NOT NULL,
-		[retryCount] INT NOT NULL,
-		[isLocking] BIT NOT NULL,
-		[lockEnd] BIGINT NOT NULL,
-		UNIQUE ([msgId])
-	);
-END;
-
-IF OBJECT_ID(N'{Options.CurrentValue.FullReceivedTableName}',N'U') IS NULL
-BEGIN
-	CREATE TABLE {Options.CurrentValue.FullReceivedTableName}
-	(
-		[id] BIGINT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-		[msgId] VARCHAR(32) NOT NULL,
-		[environment] VARCHAR(32),
-		[eventName] VARCHAR(255) NOT NULL,
-		[eventHandlerName] VARCHAR(255) NOT NULL,
-		[eventBody] NVARCHAR(MAX) NOT NULL,
-		[createTime] BIGINT NOT NULL,
-		[isDelay] BIT NOT NULL,
-		[delayAt] BIGINT NOT NULL,
-		[expireTime] BIGINT NOT NULL,
-		[eventItems] NVARCHAR(MAX) NULL,
-		[status] VARCHAR(32) NOT NULL,
-		[retryCount] INT NOT NULL,
-		[isLocking] BIT NOT NULL,
-		[lockEnd] BIGINT NOT NULL,
-		UNIQUE ([msgId], [eventHandlerName])
-	);
-END;
-";
-            await using var connection = new SqlConnection(ConnectionString.ConnectionString);
-            await connection.OpenAsync(cancellationToken).ConfigureAwait(false);
-            await using var cmd = connection.CreateCommand();
-            cmd.CommandText = sql;
-            await cmd.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
-        }
     }
 }
